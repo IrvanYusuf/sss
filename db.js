efd9c648fd70a097b6ad020a4b64cd2a6d43f30d
vanyusuf <221130031@students.mikroskil.ac.id>
2024-02-22 21:00:48 +0700

    vercel

--- db.js
+++ db.js
@@ -1,19 +1,24 @@
 const { Sequelize } = require("sequelize");
 
-const connection = new Sequelize("undangan_management", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  dialectOptions: {
-    useUTC: false,
-    dateStrings: true,
-    typeCast(field, next) {
-      if (field.type === "DATETIME") {
-        return field.string();
-      }
-      return next();
+const connection = new Sequelize(
+  "bozrfw2miwisr2bnizcq",
+  "upqvw0evryi1zmhl",
+  "vtPYNyLJ2TXjXmVTjUJr",
+  {
+    host: "bozrfw2miwisr2bnizcq-mysql.services.clever-cloud.com",
+    dialect: "mysql",
+    dialectOptions: {
+      useUTC: false,
+      dateStrings: true,
+      typeCast(field, next) {
+        if (field.type === "DATETIME") {
+          return field.string();
+        }
+        return next();
+      },
     },
-  },
-  timezone: "+07:00",
-});
+    timezone: "+07:00",
+  }
+);
 
 module.exports = connection;
