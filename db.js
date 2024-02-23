d20d0a00cd45973d676843c13ea00e397dabdf57
vanyusuf <221130031@students.mikroskil.ac.id>
2024-02-23 16:24:10 +0700

    update db

--- db.js
+++ db.js
@@ -1,26 +1,34 @@
 const { Sequelize } = require("sequelize");
+const mysql = require("mysql2/promise");
+// const connection = new Sequelize(
+//   process.env.MYSQL_ADDON_DB,
+//   process.env.MYSQL_ADDON_USER,
+//   process.env.MYSQL_ADDON_PASSWORD,
+//   {
+//     host: process.env.MYSQL_ADDON_HOST,
+//     dialect: "mysql",
+//     port: process.env.MYSQL_ADDON_PORT,
+//     password: process.env.MYSQL_ADDON_PASSWORD,
+//     dialectOptions: {
+//       useUTC: false,
+//       dateStrings: true,
+//       typeCast(field, next) {
+//         if (field.type === "DATETIME") {
+//           return field.string();
+//         }
+//         return next();
+//       },
+//     },
+//     timezone: "+07:00",
+//   }
+// );
 
-const connection = new Sequelize(
-  process.env.MYSQL_ADDON_DB,
-  process.env.MYSQL_ADDON_USER,
-  process.env.MYSQL_ADDON_PASSWORD,
-  {
-    host: process.env.MYSQL_ADDON_HOST,
-    dialect: "mysql",
-    port: process.env.MYSQL_ADDON_PORT,
-    password: process.env.MYSQL_ADDON_PASSWORD,
-    dialectOptions: {
-      useUTC: false,
-      dateStrings: true,
-      typeCast(field, next) {
-        if (field.type === "DATETIME") {
-          return field.string();
-        }
-        return next();
-      },
-    },
-    timezone: "+07:00",
-  }
-);
+const connection = mysql.createPool({
+  host: process.env.MYSQL_ADDON_HOST,
+  database: process.env.MYSQL_ADDON_DB,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  namedPlaceholders: true,
+});
 
 module.exports = connection;
